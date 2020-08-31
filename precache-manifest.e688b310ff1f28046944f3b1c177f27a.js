d3ec9b72c66de96902b305e2d70b3514958b1055
Luis <luisabad3210@gmail.com>
2020-08-31 15:53:46 -0400

    Updates

---
+++ precache-manifest.e688b310ff1f28046944f3b1c177f27a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0e83b881cb37a1297abf0b6432cc210",
+    "url": "/facebook/index.html"
+  },
+  {
+    "revision": "3d637233468844253182",
+    "url": "/facebook/static/css/main.1d71fa18.chunk.css"
+  },
+  {
+    "revision": "656c0f183429cf8b0c36",
+    "url": "/facebook/static/js/2.00b59433.chunk.js"
+  },
+  {
+    "revision": "3d637233468844253182",
+    "url": "/facebook/static/js/main.3269c83b.chunk.js"
+  },
+  {
+    "revision": "5917202065f91eb1b257",
+    "url": "/facebook/static/js/runtime-main.02a3b074.js"
+  },
+  {
+    "revision": "73588ccef59544308ec50fc2b37f9a29",
+    "url": "/facebook/static/media/ProjectOne.73588cce.png"
+  },
+  {
+    "revision": "694715a77fc3ebb644d47835f7c57a22",
+    "url": "/facebook/static/media/ProjectTwo.694715a7.png"
+  },
+  {
+    "revision": "368355e28e91d37df1daa4c47294b2bb",
+    "url": "/facebook/static/media/background.368355e2.jpg"
+  },
+  {
+    "revision": "07270c13412737933c6f3468d97618ad",
+    "url": "/facebook/static/media/css.07270c13.png"
+  },
+  {
+    "revision": "0ea316dbe0fd5dab21e160b3f48fa6b5",
+    "url": "/facebook/static/media/html.0ea316db.png"
+  },
+  {
+    "revision": "378c80aef4b3e4cbd2edb407c79a361e",
+    "url": "/facebook/static/media/image1.378c80ae.png"
+  },
+  {
+    "revision": "c66ad30b55d9395fc0413f0f14bd9730",
+    "url": "/facebook/static/media/node.c66ad30b.jpg"
+  },
+  {
+    "revision": "63fe1bd93509f9065fc5b75c7ed966b3",
+    "url": "/facebook/static/media/react.63fe1bd9.png"
+  },
+  {
+    "revision": "911d87d7c7b792f930e7f0a46b9994f0",
+    "url": "/facebook/static/media/uiux.911d87d7.png"
+  }
+]);
